5fad9ceb1843d4f5c4bbaa7160dc0cdac113b319
Nicolas Angerossa <nangerossa@frd.utn.edu.ar>
2025-06-17 17:06:16 -0300

    creo extraer chunck

--- Funciones/main.py
+++ Funciones/main.py
@@ -2,16 +2,20 @@
 Este es el archivo principal con el cual se ejecutan todos los scripts sobre el pdf
 que se obtiene de {ruta_pdf} y se guarda un archivo modificado en {guardar_en}
 """
-
+from extraer_chunk import devolver_archivo
 from procesar import procesar_pdf
 
-palabras = ["Universo", "antiguo"]
+#palabras = ["Universo", "antiguo"]
 
-ruta_pdf = "data/Input/potter.pdf"
+#ruta_pdf = "/home/nicotito/Documents/Practicas-2/Data/Input/potter.pdf"
 
-guardar_en = "data/Output"
+#guardar_en = "/home/nicotito/Documents/Practicas-2/Data/Output/"
 
-procesar_pdf(ruta_pdf, guardar_en, palabras)
+#procesar_pdf(ruta_pdf, guardar_en, palabras)
+
+guardar_en = "/home/nicotito/Documents/Practicas-2/Data/Output/potter_texto.txt"
+
+devolver_archivo(guardar_en)
 
 
 
--- Funciones/procesar.py
+++ Funciones/procesar.py
@@ -22,13 +22,8 @@ def procesar_pdf(path_pdf, guardar, palabras_claves):
     if texto:
         nombre_archivo_pdf = os.path.splitext(os.path.basename(path_pdf))[0]#obtenemos el nombre del archivo
         output_path = os.path.join(guardar, f"{nombre_archivo_pdf}_texto.txt")#creamos un .txt con el nombre del texto_texto.pdf
-        with open (output_path, "w", encoding="utf-8") as f:#abrimos el archivo y lo hacemos .txt
-            for pagina, contenido in texto.items():#por cada pagina le obtenemos el numero y el contenido
-                f.write(f"---{pagina}---\n")#le agregamos al .txt el numero de la pagina
-                f.write(f"{contenido}\n\n")# le agregamos el contenido
-        
-        texto_limpio=limpiar_texto(output_path)#llamamos a la funcion limpiar texto que devuelve un string
-                                                #con el texto sin caracteres no deseados
+        #llamamos a la funcion limpiar texto que devuelve un string
+        texto_limpio= Abrir_limpiar(output_path,texto)                         #con el texto sin caracteres no deseados
         with open (output_path, "w", encoding="utf-8") as f:
             f.write(texto_limpio)#borramos el texto anterior y le copiamos el contenido formateado
         
@@ -39,3 +34,12 @@ def procesar_pdf(path_pdf, guardar, palabras_claves):
             
     else:
         print("Sin contenido")            
+
+def Abrir_limpiar(output_path, texto):
+    with open (output_path, "w", encoding="utf-8") as f:#abrimos el archivo y lo hacemos .txt
+            for pagina, contenido in texto.items():#por cada pagina le obtenemos el numero y el contenido
+                f.write(f"---{pagina}---\n")#le agregamos al .txt el numero de la pagina
+                f.write(f"{contenido}\n\n")# le agregamos el contenido
+        
+    texto_limpio=limpiar_texto(output_path)
+    return texto_limpio
